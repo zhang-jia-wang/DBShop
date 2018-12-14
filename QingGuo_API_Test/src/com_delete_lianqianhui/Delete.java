c960d61aef480cb55c90b81ebb29a19a46fd2a4a
LianQianhui1998 <18889811131@163.com>
2018-12-14 10:54:40 +0800

    change

--- QingGuo_API_Test/src/com_delete_lianqianhui/Delete.java
+++ QingGuo_API_Test/src/com_delete_lianqianhui/Delete.java
@@ -1,5 +1,22 @@
 package com_delete_lianqianhui;
 
-public class Delete {
+import static org.testng.Assert.assertEquals;
 
+import java.io.IOException;
+
+import org.apache.http.client.CookieStore;
+import org.testng.annotations.Test;
+import net.sf.json.JSONObject;
+
+public class Delete {
+	@Test
+	public void Delete_Test() throws IOException, Exception {
+			CookieStore cookie = Common.getLoginCookie(null, null);
+			JSONObject dele=new JSONObject();
+			dele.element("id","82316007");//id 只能手动获取
+			String result=HttpDriver.doPost("http://study-perf.qa.netease.com/fgadmin/address/delete", dele, cookie);
+			JSONObject json=JSONObject.fromObject(result);
+			assertEquals(json.getString("message"),"success");
+
+	}
 }
