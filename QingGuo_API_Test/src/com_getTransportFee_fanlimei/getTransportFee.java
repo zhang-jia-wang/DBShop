331784e24cbf796e4f1c5e49eb0bd281f8909628
mengyuxuan0528 <945248846@qq.com>
2018-12-17 16:39:51 +0800

    201811111111

--- QingGuo_API_Test/src/com_getTransportFee_fanlimei/getTransportFee.java
+++ QingGuo_API_Test/src/com_getTransportFee_fanlimei/getTransportFee.java
@@ -1,8 +1,33 @@
 package com_getTransportFee_fanlimei;
 
+import org.testng.annotations.Test;
+
+import com.edu.core.HttpDriver;
+
+import net.sf.json.JSONObject;
+
 public class getTransportFee {
-	public void getTransportFeeTest() {
-		String url=" /common/getTransportFee";
-		
+	public double transportFee;
+	@Test
+	public void getTransportFeeTest1() throws Exception {
+//		String url=" /common/getTransportFee";
+			String result_fee= HttpDriver.doGet("http://study-perf.qa.netease.com/common/getTransportFee?id=1&addressDetail=%E6%B1%9F%E8%8B%8F%E7%9C%81_%E5%8D%97%E4%BA%AC%E5%B8%82_%E9%BC%93%E6%A5%BC%E5%8C%BA&tm=1540950077650");
+			JSONObject json = JSONObject.fromObject(result_fee);
+			transportFee=json.getInt("result");
+			System.out.println("тк╥я:"+transportFee);		
+	}
+	
+	@Test
+	public void getTransportFeeTest2() throws Exception {
+			String result_fee= HttpDriver.doGet("http://study-perf.qa.netease.com/common/getTransportFee?id=null&addressDetail=%E6%B1%9F%E8%8B%8F%E7%9C%81_%E5%8D%97%E4%BA%AC%E5%B8%82_%E9%BC%93%E6%A5%BC%E5%8C%BA&tm=1540950077650");
+			System.out.println(result_fee);
+			
+	}
+	
+	@Test
+	public void getTransportFeeTest3() throws Exception {
+			String result_fee= HttpDriver.doGet("http://study-perf.qa.netease.com/common/getTransportFee?addressDetail=%E6%B1%9F%E8%8B%8F%E7%9C%81_%E5%8D%97%E4%BA%AC%E5%B8%82_%E9%BC%93%E6%A5%BC%E5%8C%BA&tm=1540950077650");
+			System.out.println(result_fee);
+			
 	}
 }
